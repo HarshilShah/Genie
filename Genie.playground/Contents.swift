1e6c8fa9b3ba90e799e5c80b64d0454eba5cbc32
Harshil Shah <harshilshah1910@outlook.com>
2021-04-11 17:36:23 +0530

    Initial setup

--- Genie.playground/Contents.swift
+++ Genie.playground/Contents.swift
@@ -1,2 +1,17 @@
 import SpriteKit
 import PlaygroundSupport
+
+let frame = CGRect(x: 0, y: 0, width: 800, height: 600)
+let skView = SKView(frame: frame)
+skView.appearance = NSAppearance(named: .aqua)
+PlaygroundPage.current.liveView = skView
+
+let scene = SKScene(size: frame.size)
+scene.backgroundColor = .windowBackgroundColor
+
+let imageNode = SKSpriteNode(imageNamed: "SysPrefs.png")
+imageNode.position = CGPoint(x: frame.midX, y: frame.midY)
+imageNode.size = frame.size
+scene.addChild(imageNode)
+
+skView.presentScene(scene)
