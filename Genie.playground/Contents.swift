7d39beb3dc6fc1275dc51ca3524d9a2712ac0d01
Harshil Shah <harshilshah1910@outlook.com>
2021-04-17 18:51:30 +0530

    Variable fix

--- Genie.playground/Contents.swift
+++ Genie.playground/Contents.swift
@@ -1,11 +1,15 @@
 import SpriteKit
 import PlaygroundSupport
 
+enum GenieAnimationEdge {
+	case top
+}
+
 enum GenieAnimationDirection {
 	case minimize, maximize
 }
 
-func genie(maximized: CGRect, minimized: CGRect, direction: GenieAnimationDirection) -> SKAction {
+func genie(maximized: CGRect, minimized: CGRect, direction: GenieAnimationDirection, edge: GenieAnimationEdge) -> SKAction {
 	let slideAnimationEndFraction = 0.5
 	let translateAnimationStartFraction = 0.4
 	
@@ -15,66 +19,71 @@ func genie(maximized: CGRect, minimized: CGRect, direction: GenieAnimationDirect
 
 	let rowCount = 50
 	
-	let leftBezierTopX = Double(maximized.minX)
-	let rightBezierTopX = Double(maximized.maxX)
-	
-	let leftEdgeDistanceToMove = Double(minimized.minX - maximized.minX)
-	let rightEdgeDistanceToMove = Double(minimized.maxX - maximized.maxX)
-	let verticalDistanceToMove = Double(minimized.maxY - maximized.maxY)
-	
-	let bezierTopY = Double(maximized.maxY)
-	let bezierBottomY = Double(minimized.maxY)
-	let bezierHeight = bezierTopY - bezierBottomY
-	
-	let positions: [[SIMD2<Float>]] = stride(from: 0, to: frameCount, by: 1).map { frame in
-		let fraction = (frame / (frameCount - 1))
-		let slideProgress = max(0, min(1, fraction/slideAnimationEndFraction))
-		let translateProgress = max(0, min(1, (fraction - translateAnimationStartFraction)/(1 - translateAnimationStartFraction)))
-		
-		let translation = translateProgress * verticalDistanceToMove
-		let topEdgeVerticalPosition = Double(maximized.maxY) + translation
-		let bottomEdgeVerticalPosition = max(
-			Double(maximized.minY) + translation,
-			Double(minimized.minY)
-		)
-		
-		let leftBezierBottomX = leftBezierTopX + (slideProgress * leftEdgeDistanceToMove)
-		let rightBezierBottomX = Double(maximized.maxX) + (slideProgress * rightEdgeDistanceToMove)
-		
-		func leftBezierPosition(forY y: Double) -> Double {
-			switch y {
-			case ..<bezierBottomY:
-				return leftBezierBottomX
-			case bezierBottomY ..< bezierTopY:
-				let progress = ((y - bezierBottomY) / bezierHeight).quadraticEaseInOut
-				return (progress * (leftBezierTopX - leftBezierBottomX)) + leftBezierBottomX
-			default:
-				return leftBezierTopX
+	let positions: [[SIMD2<Float>]] = {
+		switch edge {
+		case .top:
+			let leftBezierTopX = Double(maximized.minX)
+			let rightBezierTopX = Double(maximized.maxX)
+			
+			let leftEdgeDistanceToMove = Double(minimized.minX - maximized.minX)
+			let rightEdgeDistanceToMove = Double(minimized.maxX - maximized.maxX)
+			let verticalDistanceToMove = Double(minimized.maxY - maximized.maxY)
+			
+			let bezierTopY = Double(maximized.maxY)
+			let bezierBottomY = Double(minimized.maxY)
+			let bezierHeight = bezierTopY - bezierBottomY
+			
+			return stride(from: 0, to: frameCount, by: 1).map { frame in
+				let fraction = (frame / (frameCount - 1))
+				let slideProgress = max(0, min(1, fraction/slideAnimationEndFraction))
+				let translateProgress = max(0, min(1, (fraction - translateAnimationStartFraction)/(1 - translateAnimationStartFraction)))
+				
+				let translation = translateProgress * verticalDistanceToMove
+				let topEdgeVerticalPosition = Double(maximized.maxY) + translation
+				let bottomEdgeVerticalPosition = max(
+					Double(maximized.minY) + translation,
+					Double(minimized.minY)
+				)
+				
+				let leftBezierBottomX = leftBezierTopX + (slideProgress * leftEdgeDistanceToMove)
+				let rightBezierBottomX = rightBezierTopX + (slideProgress * rightEdgeDistanceToMove)
+				
+				func leftBezierPosition(forY y: Double) -> Double {
+					switch y {
+					case ..<bezierBottomY:
+						return leftBezierBottomX
+					case bezierBottomY ..< bezierTopY:
+						let progress = ((y - bezierBottomY) / bezierHeight).quadraticEaseInOut
+						return (progress * (leftBezierTopX - leftBezierBottomX)) + leftBezierBottomX
+					default:
+						return leftBezierTopX
+					}
+				}
+				
+				func rightBezierPosition(forY y: Double) -> Double {
+					switch y {
+					case ..<bezierBottomY:
+						return rightBezierBottomX
+					case bezierBottomY ..< bezierTopY:
+						let progress = ((y - bezierBottomY) / bezierHeight).quadraticEaseInOut
+						return (progress * (rightBezierTopX - rightBezierBottomX)) + rightBezierBottomX
+					default:
+						return rightBezierTopX
+					}
+				}
+				
+				return (0 ... rowCount)
+					.map { Double($0) / Double(rowCount) }
+					.flatMap { position -> [SIMD2<Double>] in
+						let y = (topEdgeVerticalPosition * position) + (bottomEdgeVerticalPosition * (1 - position))
+						let xMin = leftBezierPosition(forY: y)
+						let xMax = rightBezierPosition(forY: y)
+						return [SIMD2(xMin, y), SIMD2(xMax, y)]
+					}
+					.map(SIMD2<Float>.init)
 			}
 		}
-		
-		func rightBezierPosition(forY y: Double) -> Double {
-			switch y {
-			case ..<bezierBottomY:
-				return rightBezierBottomX
-			case bezierBottomY ..< bezierTopY:
-				let progress = ((y - bezierBottomY) / bezierHeight).quadraticEaseInOut
-				return (progress * (rightBezierTopX - rightBezierBottomX)) + rightBezierBottomX
-			default:
-				return rightBezierTopX
-			}
-		}
-		
-		return (0 ... rowCount)
-			.map { Double($0) / Double(rowCount) }
-			.flatMap { position -> [SIMD2<Double>] in
-				let y = (topEdgeVerticalPosition * position) + (bottomEdgeVerticalPosition * (1 - position))
-				let xMin = leftBezierPosition(forY: y)
-				let xMax = rightBezierPosition(forY: y)
-				return [SIMD2(xMin, y), SIMD2(xMax, y)]
-			}
-			.map(SIMD2<Float>.init)
-	}
+	}()
 	
 	let orientedPositions = direction == .minimize ? positions : positions.reversed()
 	
@@ -122,5 +131,5 @@ imageNode.warpGeometry = SKWarpGeometryGrid(
 let finalFrame = CGRect(x: 640, y: 0, width: 50, height: 50)
 	.normalized(in: skView.frame)
 
-imageNode.run(genie(maximized: initialFrame, minimized: finalFrame, direction: .minimize))
-imageNode.run(genie(maximized: initialFrame, minimized: finalFrame, direction: .maximize))
+imageNode.run(genie(maximized: initialFrame, minimized: finalFrame, direction: .minimize, edge: .top))
+imageNode.run(genie(maximized: initialFrame, minimized: finalFrame, direction: .maximize, edge: .top))
